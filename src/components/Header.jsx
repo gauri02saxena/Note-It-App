3083ea04423bb9d33868b46104df7088dc704d46
gauri02saxena <gauri.saxena7379@gmail.com>
2023-08-20 21:41:41 +0530

    Complete NotIt app

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import EditNoteIcon from '@mui/icons-material/EditNote';
+
+function Header() {
+  return (
+    <header>
+      <h1> <EditNoteIcon /> Note It</h1>
+    </header>
+  );
+}
+
+export default Header;
